e283052563608720f5e2411755283ffb306bbde0
Andre <andre.basche@gmail.com>
2018-12-26 01:34:24 +0100

    Read FHEM Sensors

--- state_channel.py
+++ state_channel.py
@@ -2,7 +2,9 @@ import logging
 import os
 import sys
 from datetime import datetime, timedelta
+from pprint import pprint
 
+import fhem as fhem
 from telegram.bot import Bot
 from telegram.ext.callbackqueryhandler import CallbackQueryHandler
 from telegram.ext.updater import Updater
@@ -11,7 +13,7 @@ from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 from telegram.parsemode import ParseMode
 
 from basic_mongo import BasicMongo as mongo
-from secrets import TELEGRAM, DEBUG
+from secrets import TELEGRAM, DEBUG, FHEM2, FHEM_DEVICES
 
 if DEBUG:
     logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, format='%(levelname)s - %(message)s')
@@ -24,9 +26,15 @@ logger = logging.getLogger(__name__)
 def parse_message():
     text = []
     db = mongo.get_db()
+    fh = fhem.Fhem(FHEM2['server'], port=FHEM2['port'], protocol=FHEM2['protocol'], loglevel=99,
+                   username=FHEM2['username'], password=FHEM2['password'])
     state = {state['_id']: [state['last']['state'], state['last']['timestamp']] for state in mongo.get_last(db)}
+    for idx, v in enumerate(fh.get_readings('state', filters={'subType': 'threeStateSensor'}).values()):
+        state['schuppen-{}'.format(idx)] = [True if v['Value'] == 'open' else False, v['Time']]
     now = datetime.utcnow()
-    for group, x in sorted(mongo.get_names(db).items()):
+    groups = mongo.get_names(db)
+    groups.update(FHEM_DEVICES)
+    for group, x in sorted(groups.items()):
         text.append("\n*{}*".format(group))
         for s_id, name in sorted(x.items(), key=lambda x: x[1][0]):
             name = "*{}*".format(name[0]) if (state[s_id][1] + timedelta(minutes=1)) > now else name[0]
